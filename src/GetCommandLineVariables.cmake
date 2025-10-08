ff3dac6e147ae81db873d1cafb3c5e5b8bf04794
Ogoruwa <156860592+Ogoruwa@users.noreply.github.com>
2025-10-08 13:45:06 +0100

    Added quoting for filepath variables parsed by `GetCommandLineVariales.cmake`

--- src/GetCommandLineVariables.cmake
+++ src/GetCommandLineVariables.cmake
@@ -18,7 +18,17 @@ foreach(CACHE_VAR ${CACHE_VARS})
             set(CACHE_VAR_TYPE :${CACHE_VAR_TYPE})
         endif()
 
-        list(APPEND CMAKE_ARGS "-D${CACHE_VAR}${CACHE_VAR_TYPE}=${${CACHE_VAR}}")
+        if(${${CACHE_VAR}} STREQUAL "")
+            set(CACHE_VAR_VALUE "\"\"")
+        else()
+            set(CACHE_VAR_VALUE "${${CACHE_VAR}}")
+        endif()
+
+        if(CACHE_VAR_TYPE STREQUAL ":FILEPATH")
+            set(CACHE_VAR_VALUE "'${CACHE_VAR_VALUE}'")
+        endif()
+
+        list(APPEND CMAKE_ARGS "-D${CACHE_VAR}${CACHE_VAR_TYPE}=${CACHE_VAR_VALUE}")
     endif()
 
 endforeach()
